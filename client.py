878b8cad31ce1aed8616231565799c84f6257b46
Adam Poszyler <adamposzyler@gmail.com>
2022-11-07 13:06:58 +0100

    init changes

--- client.py
+++ client.py
@@ -21,7 +21,7 @@ class Player():
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.shape)
 
-    def move(self, SIZE):
+    def move(self, WIN_SIZE):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
             if self.x <= 0:
@@ -29,15 +29,15 @@ class Player():
             else:
                 self.x -= self.velocity
         if keys[pygame.K_RIGHT]:
-            if self.x >= SIZE - self.width:
-                self.x = SIZE - self.width
+            if self.x >= WIN_SIZE - self.width:
+                self.x = WIN_SIZE - self.width
             else:
                 self.x += self.velocity
 
+            self.update()
+        
+    def update(self):
         self.shape = pygame.Rect(self.x, self.y, self.width, self.height)
-
-    def bounds(self):
-        pass
 
 
 def send(client, mes):
@@ -47,10 +47,10 @@ def send(client, mes):
 
 
 def main_game():
-    SIZE = 500
+    WIN_SIZE = 500
     pygame.init()
     pygame.display.set_caption("Game")
-    screen = pygame.display.set_mode((SIZE, SIZE))
+    screen = pygame.display.set_mode((WIN_SIZE, WIN_SIZE))
 
     player = Player(50, 50, 200, 50, (0, 0, 0))
     clock = pygame.time.Clock()
@@ -62,7 +62,7 @@ def main_game():
                 running = False
                 pygame.quit()
                 quit()
-        player.move(SIZE)
+        player.move(WIN_SIZE)
         redraw_window(screen, player)
 
 
--- server.py
+++ server.py
@@ -8,14 +8,14 @@ HOST = "127.0.0.1"
 FORMAT = 'utf-8'
 
 
-def handle_client(conn, addr, num_of_clients):
+def handle_client(conn, addr, client_number):
     print(f"[NEW CONNECTION {addr} connected.")
     connected = True
     while connected:
-        if num_of_clients > 2:
-            conn.send("stop".encode(FORMAT))
-            connected = False
-            continue
+        # if client_number > 2:
+        #     conn.send("stop".encode(FORMAT))
+        #     connected = False
+        #     continue
 
         message = conn.recv(SIZE).decode(FORMAT)
         if message == "stop":
@@ -28,10 +28,10 @@ def handle_client(conn, addr, num_of_clients):
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
     server.bind((HOST, PORT))
-    server.listen()
+    server.listen(2)
     while True:
         conn, addr = server.accept()
-        num_of_clients = threading.activeCount()
-        thread = threading.Thread(target=handle_client, args=(conn, addr, num_of_clients))
+        client_number = threading.activeCount()
+        thread = threading.Thread(target=handle_client, args=(conn, addr, client_number))
         thread.start()
         print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
